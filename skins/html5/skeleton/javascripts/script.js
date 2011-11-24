837b55eca62fa72337746403c497cc5f267db8aa
Vladimir Dronnikov <dronnikov@gmail.com>
2011-11-24 20:17:03 +0400

    start to move presentation to jade

---
+++ skins/html5/skeleton/javascripts/script.js
@@ -0,0 +1,8 @@
+$(function(){
+  // setup code prettifier
+  //$('pre[class!=syntax] >code').addClass('prettyprint');
+  //$('#home pre >code').removeClass('prettyprint');
+  prettyPrint();
+  // honor initial hash
+  //hashChanged(location.href.replace(/(^.*)#/, '#'));
+});
