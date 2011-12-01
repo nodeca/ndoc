f4fbbc8493637f0fa0427c6f11c43d4c466dd803
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-01 20:44:16 +0100

    Fixes to current item css class

--- skins/default/skeleton/javascripts/script.js
+++ skins/default/skeleton/javascripts/script.js
@@ -12,13 +12,27 @@ $(function () {
       targets = [], // items and corresponding article offset
       $active = null; // active menu item
 
+  function eachParent($item, callback) {
+    var $parent = $item.data('ndoc.parent');
+    if ($parent && $parent.length) {
+      eachParent($parent, callback);
+      callback($parent);
+    }
+  }
 
   // activates item (used upon scrolling)
   function activate($item) {
-    $item.data('ndoc.childs').animate({
-      height: 'show',
-      opacity: 'show'
-    }, 'fast');
+    if ($active) {
+      $active.removeClass('current');
+      eachParent($active, function ($parent) {
+        $parent.removeClass('current-parent');
+      });
+    }
+
+    $active = $item.addClass('current');
+    eachParent($active, function ($parent) {
+      $parent.addClass('current-parent');
+    });
   }
 
 
@@ -37,14 +51,17 @@ $(function () {
 
   $items.each(function () {
     var $item = $(this),
-        $childs = $item.parent().next();
+        $childs = $item.parent().next(),
+        $parent = $item.parents('ul').eq(0).prev().children();
 
     // cross-ref tree
-    $item.data('ndoc.parent', $item.parents('ul').eq(0).prev().children());
+    $item.data('ndoc.parent', $parent);
     $item.data('ndoc.childs', $childs);
 
     // bind activator
     $item.click(function () {
+      activate($item);
+
       $childs.stop().toggleData('ndoc.collapsed').animate({
         height: 'toggle',
         opacity: 'toggle'
@@ -62,12 +79,14 @@ $(function () {
       offset: $('[id="' + $item.attr('href').slice(1) + '"]').offset().top
     }, 'fast');
 
-    // collapse all by default
-    $childs.data('ndoc.collapsed', true).hide();
+    // collapse all 2nd levels
+    if (0 != $parent.length) {
+      $childs.data('ndoc.collapsed', true).hide();
+    }
   });
 
 
   // activate scroll spy
-  $window.scroll(processScroll);
+  $window.scroll(function () { $.throttle(250, processScroll); });
   processScroll();
 });
