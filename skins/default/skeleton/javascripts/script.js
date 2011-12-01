8d25107d74887f7ade26e509c267fa0f67ff5a9c
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-01 15:13:00 +0100

    Implements scrollspy sketch

--- skins/default/skeleton/javascripts/script.js
+++ skins/default/skeleton/javascripts/script.js
@@ -1,19 +1,67 @@
 $(function () {
   'use strict';
 
-  $('div.menu-item > a').each(function () {
-    var $item = $(this),
-        $child = $item.parent().next();
+  var $window = $(window),
+      $items = $('div.menu-item > a'),
+      targets = [],
+      $active = null;
 
+  function toggle($item) {
+    $item.data('ndoc.menu-child').stop().toggleClass('collapsed').toggle('slow');
+  }
+
+  function deactivate($item) {
+    $item.removeClass('active');
+  }
+
+  function activate($item) {
+    if ($active) {
+      deactivate($active);
+    }
+
+    $active = $item.addClass('active');
+  }
+
+  function processScroll() {
+    var scrollTop = $window.scrollTop() + 10,
+        i = targets.length;
+    
+    while (i--) {
+      $active !== targets[i].item
+        && scrollTop >= targets[i].offset
+        && (!targets[i + 1].offset || scrollTop <= targets[i + 1].offset)
+        && activate(targets[i].item);
+    }
+  }
+
+  $items.each(function () {
+    var $item = $(this),
+        $child = $item.parent().next(),
+        $article = $('[id="' + $item.attr('href').slice(1) + '"]');
+
+    // store cross references
+    $item.data('ndoc.menu-child', $child);
+    $child.data('ndoc.menu-parent', $item);
+
+    // bind activator
     $item.click(function () {
-      $child.stop().toggleClass('collapsed').toggle('normal');
+      toggle($item);
       if ($child.hasClass('collapsed')) {
         // prevent from switching to article
         return false;
       }
     });
 
+    // fill-in article offset
+    targets.push({
+      offset: $article.offset().top,
+      item: $item
+    });
+
     // collapse all by default
-    $child.addClass('collapsed').hide('slow');
+    toggle($item);
   });
+
+
+  $window.scroll(processScroll);
 });
