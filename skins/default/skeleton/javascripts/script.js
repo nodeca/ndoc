f6b8994ea1011b6e7aab422616ddce5132233a03
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-02 01:30:23 +0100

    Disbled scroll spy

--- skins/default/skeleton/javascripts/script.js
+++ skins/default/skeleton/javascripts/script.js
@@ -121,8 +121,7 @@ $(function () {
       // click - cuz i don't know what event fied on input clear in Chrome
       .on('change click', updateSearchResults);
 
-  // activate scroll spy
-  $window.scroll($.throttle(250, processScroll));
+  // initial jump (for permalinks)
   processScroll();
 
   // init prettyprint
