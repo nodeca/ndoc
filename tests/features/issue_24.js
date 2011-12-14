0184c6baade9e8f97121fd1a9fae8b9f67066826
Aleksey V Zapparov <ixti@member.fsf.org>
2011-12-14 13:45:16 +0100

    Features tests restructured

---
+++ tests/features/issue_24.js
@@ -0,0 +1,6 @@
+/**
+ *  Features.each(obj, iterator(val, key)[, callback(err, result)])
+ *
+ *  We support really free format of signatures. Now you can specify arguments
+ *  while you specifying arguments...
+ **/
