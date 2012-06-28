f4fba4a1ca5d3668e09aaa9d7acc6ae5953c93dc
Aleksey V Zapparov <ixti@member.fsf.org>
2012-06-28 16:46:00 +0200

    Fixes #2

---
+++ tests/features/issue_2.js
@@ -0,0 +1,17 @@
+/**
+ *  Issue_2
+ *
+ *  ##### long <code> lines splitted in Chrome/Opera after highlight
+ *
+ *  Long lines in code blocks are splitted, no horizontal scroll appears. Any
+ *  ideas how to fix?
+ *
+ *  Reproduced in Chrome/Opera, no such problem in FF.
+ *
+ *  ##### Test
+ *
+ *      var a = 'foo' + 'bar' + 'baz',
+ *          b = function mege_long_function(withLots, ofArguments, thatWill, causeA, bug) {
+ *            // gotcha
+ *          };
+ **/
