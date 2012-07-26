63f4d09ff7ecad6231759a439429ed3016924b16
Oleg Efimov <efimovov@gmail.com>
2012-07-27 02:56:03 +0400

    Test case with section -> class -> event example

---
+++ tests/features/issue_63.js
@@ -0,0 +1,15 @@
+/**
+ * == Issue62 ==
+ **/
+
+/** section: Issue62
+ * class PullRequest
+ *
+ * This is a pull request that fixed "section > class > event" name parsing
+ **/
+
+/**
+ * PullRequest@event()
+ *
+ * Event example
+ **/
